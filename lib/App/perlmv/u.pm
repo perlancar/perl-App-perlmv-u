f4b35e4b3de9261ef5b46aef53b10cf6f5f952ca
perlancar (on pc-office) <perlancar@gmail.com>
2017-07-28 18:28:18 +0700

    Release v0.001

--- lib/App/perlmv/u.pm
+++ lib/App/perlmv/u.pm
@@ -9,6 +9,44 @@ use Log::ger;
 
 our %SPEC;
 
+# for now we're not using a proper RM, we just record undo actions in
+# ~/.perlmv-u.undo.dat
+
+$SPEC{':package'} = {
+    v => 1.1,
+    summary => 'Rename files using Perl code, with undo/redo',
+};
+
+sub _undo_file_path {
+    $ENV{HOME} . "/.perlmv-u.undo.dat";
+}
+
+sub _read_undo_file {
+    require JSON::PP;
+    my $path = _undo_file_path();
+    if (-e $path) {
+        my $content = do {
+            local $/;
+            open my $fh, "<", $path
+                or die "Can't open undo file '$path' for reading: $!";
+            scalar <$fh>;
+        };
+        return JSON::PP::decode_json($content);
+    } else {
+        return [];
+    }
+}
+
+sub _write_undo_file {
+    require JSON::PP;
+
+    my $path = _undo_file_path();
+    open my $fh, ">", $path
+        or die "Can't open undo file '$path' for writing: $!";
+    print $fh JSON::PP::encode_json($_[0]);
+    close $fh or die "Can't write undo file '$path': $!";
+}
+
 $SPEC{move_multiple} = {
     v => 1.1,
     args => {
@@ -34,10 +72,81 @@ _
     },
 };
 sub move_multiple {
+    require File::MoreUtil;
+
+    my %args = @_;
+
+    my $tx_action = $args{-tx_action};
+    if ($tx_action eq 'check_state') {
+        my (%src, %dest, %exists);
+        for my $pair (@{ $args{pairs} }) {
+            my ($src, $dest) = @$pair;
+            $src {$src}++;
+            $dest{$dest}++;
+            for my $k ($src, $dest) {
+                unless (exists $exists{$k}) {
+                    $exists{$k} = File::MoreUtil::file_exists($k);
+                }
+            }
+        }
+        my $all_dest_exist = 1;
+        for (keys %dest) {
+            unless ($exists{$_}) { $all_dest_exist = 0; last }
+        }
+        my $all_src_not_in_dest_not_exist = 1;
+        for (keys %src) {
+            next if $dest{$_};
+            if ($exists{$_}) { $all_src_not_in_dest_not_exist = 0; last }
+        }
+        if ($all_dest_exist && $all_src_not_in_dest_not_exist) {
+            # fixed
+            return [304, "All sources do not exist and ".
+                        "all targets already exist"];
+        }
+        my $all_src_exist = 1;
+        for (keys %src) {
+            unless ($exists{$_}) { $all_src_exist = 0; last }
+        }
+        my $all_dest_not_in_src_not_exist = 1;
+        for (keys %dest) {
+            next if $src{$_};
+            if ($exists{$_}) { $all_dest_not_in_src_not_exist = 0; last }
+        }
+        if ($all_src_exist && $all_dest_not_in_src_not_exist) {
+            # fixable
+            my @do_actions;
+            my @undo_actions;
+            my @pairs;
+            for my $pair (reverse @{ $args{pairs} }) {
+                push @pairs, [$pair->[1] => $pair->[0]];
+            }
+            push @do_actions  , ['move_multiple', {pairs => $args{pairs}}];
+            push @undo_actions, ['move_multiple', {pairs => \@pairs}];
+            return [200, "OK", undef, {
+                do_actions  =>\@do_actions,
+                undo_actions=>\@undo_actions}];
+        } else {
+            # not fixable
+            return [412, "Either some sources do not exist or ".
+                        "some targets exist already"];
+        }
+    } elsif ($tx_action eq 'fix_state') {
+        for my $pair (@{ $args{pairs} }) {
+            my ($src, $dest) = @$pair;
+            log_info("Renaming %s -> %s ...", $src, $dest);
+            unless (rename $src, $dest) {
+                return [500, "Can't rename '$src' -> '$dest': $!"] if $!;
+            }
+        }
+        [200, "OK"];
+    } else {
+        return [400, "Invalid -tx_action"];
+    }
 }
 
 $SPEC{perlmv} = {
     v => 1.1,
+    summary => 'Rename files using Perl code, with undo/redo',
     args => {
         eval => {
             summary => 'Perl code to rename file',
@@ -60,6 +169,10 @@ _
             pos => 0,
             greedy => 1,
         },
+        d => {
+            summary => 'Alias for --dry-run',
+            schema => ['bool*', is=>1],
+        },
     },
     features => {
         dry_run => 1,
@@ -68,8 +181,11 @@ _
 sub perlmv {
     require Cwd;
     require File::MoreUtil;
+    require String::Elide::FromArray;
 
     my %args = @_;
+
+    my $dry_run = $args{d} || $args{-dry_run};
 
     my @pairs;
     my $compiled_code;
@@ -108,11 +224,140 @@ sub perlmv {
         $exists{$absnew}++;
         push @pairs, [$absfile, $absnew];
     }
-    [200, \@pairs];
+
+    if ($dry_run) {
+        for my $pair (@pairs) {
+            log_info("[DRY-RUN] Renaming %s -> %s ...", $pair->[0], $pair->[1]);
+        }
+        return [200, "OK (dry-run)"];
+    }
+
+    my $undo = _read_undo_file();
+    my $res = move_multiple(pairs => \@pairs, -tx_action => 'check_state');
+    return $res unless $res->[0] == 200;
+    unshift @$undo, {
+        time => time(),
+        summary => "Rename ".scalar(@pairs)." file(s): ".
+            String::Elide::FromArray::elide(
+                [map { my $n = $_->[0]; $n =~ s!.+/!!; $n } @pairs], 70,
+                {max_items => 7}),
+        do_actions   => $res->[3]{'do_actions'},
+        undo_actions => $res->[3]{'undo_actions'},
+        status => 'done',
+    };
+    _write_undo_file($undo);
+    move_multiple(pairs => \@pairs, -tx_action => 'fix_state');
+}
+
+$SPEC{undo} = {
+    v => 1.1,
+    summary => 'Undo last action',
+};
+sub undo {
+    my %args = @_;
+
+    my $undo = _read_undo_file();
+    my $index;
+    for my $i (0..$#{$undo}) {
+        if ($undo->[$i]{status} eq 'done') {
+            $index = $i;
+            last;
+        }
+    }
+    unless (defined $index) {
+        return [412, "No action to undo".
+                    (!@$undo ? " (undo history is empty)" :
+                     " (all actions have been undone)")
+            ];
+    }
+
+    # sanity check: we can only handle undo_action as a single call to
+    # move_multiple
+    my $actions = $undo->[$index]{undo_actions};
+    @$actions == 1 && $actions->[0][0] eq 'move_multiple' or
+        return [412, "Can't undo (index=$index, ERR_ID=1)"];
+
+    my $res = move_multiple(%{$actions->[0][1]}, -tx_action=>'fix_state');
+    return $res unless $res->[0] == 200;
+
+    $undo->[$index]{status} = 'undone';
+    _write_undo_file($undo);
+    [200, "OK"];
+}
+
+$SPEC{redo} = {
+    v => 1.1,
+    summary => 'Redo last undone action',
+};
+sub redo {
+    my %args = @_;
+
+    my $undo = _read_undo_file();
+    my $index;
+    for my $i (reverse 0..$#{$undo}) {
+        if ($undo->[$i]{status} eq 'undone') {
+            $index = $i;
+            last;
+        }
+    }
+    unless (defined $index) {
+        return [412, "No action to redo".
+                    (!@$undo ? " (undo history is empty)" :
+                     " (all actions have been redone)")
+            ];
+    }
+
+    # sanity check: we can only handle undo_action as a single call to
+    # move_multiple
+    my $actions = $undo->[$index]{do_actions};
+    @$actions == 1 && $actions->[0][0] eq 'move_multiple' or
+        return [412, "Can't undo (index=$index, ERR_ID=1)"];
+
+    my $res = move_multiple(%{$actions->[0][1]}, -tx_action=>'fix_state');
+    return $res unless $res->[0] == 200;
+
+    $undo->[$index]{status} = 'done';
+    _write_undo_file($undo);
+    [200, "OK"];
+}
+
+$SPEC{history} = {
+    v => 1.1,
+    summary => 'Show undo history',
+};
+sub history {
+    require POSIX;
+
+    my %args = @_;
+
+    my $undo = _read_undo_file();
+    my $resmeta = {'table.fields' => [qw/time summary status/]};
+    my @res;
+    for (@$undo) {
+        push @res, {
+            time => POSIX::strftime("%Y-%m-%dT%H:%M:%S", localtime $_->{time}),
+            summary => $_->{summary},
+            status => $_->{status},
+        };
+    }
+    [200, "OK", \@res, $resmeta];
+}
+
+$SPEC{clear_history} = {
+    v => 1.1,
+    summary => 'Clear undo history',
+};
+sub clear_history {
+    require POSIX;
+
+    my %args = @_;
+
+    unlink _undo_file_path();
+    [200, "OK"];
 }
 
 1;
-# ABSTRACT: Rename files using Perl code, with undo/redo
+# ABSTRACT:
 
 =head1 DESCRIPTION
 
